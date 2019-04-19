e794870c226e2a7de489f7e872d9410dffefc477
Safiy Cham <csafiy@yahoo.com>
2019-04-19 08:55:06 -0700

    Initiate project setup

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Safi's Market</h1>
+        <div>intro section</div>
+        <h3>day selection</h3>
+        <div>data</div>
+        <h3>month selection</h3>
+        <div>data</div>
+        <footer></footer>
+    </div>,
+    document.getElementById('react-app-root')
+);

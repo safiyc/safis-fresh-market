d1ff57e41753ecdae56dc2ff93bf7e31a438202f
Safiy Cham <csafiy@yahoo.com>
2019-04-25 12:32:47 -0700

    Display nested data on page

--- src/components/MonthData.jsx
+++ src/components/MonthData.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function MonthData() {
-  return <div>data from month's selection</div>;
+function MonthData(props) {
+  return (
+    <div>
+      <hr />
+      <p>Month: {props.month}</p>
+      <p>Selection: {props.selection}</p>
+    </div>
+  );
 }
+
+MonthData.propTypes = {
+  month: PropTypes.string,
+  selection: PropTypes.string
+};
 
 export default MonthData;

05672063ba8b43d491f79de3c349a781ee040fe7
Safiy Cham <csafiy@yahoo.com>
2019-06-06 19:57:01 -0700

    Deploy, please

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-let gulp = require('gulp');
-let deploy = require('gulp-gh-pages');
-
-// this will push build dir to gh-pages
-gulp.task('deploy', function () {
-  return gulp.src('./build/**/*')
-    .pipe(deploy());
-});
